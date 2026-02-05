01529c2c2977182052bc4ae192cc84da1214b2f9
DevGuava <demongamer2006.2@gmail.com>
2026-02-05 17:17:16 -0500

    add session 2

---
+++ MTEC 1201 Session 2/sketch.js
@@ -0,0 +1,30 @@
+/*
+Session 2 by Justin Roldan
+https://p5js.org/reference/
+*/
+function setup()
+{
+  createCanvas(400, 400);
+  background(255, 120, 50);
+
+  strokeWeight(8);
+  stroke(211,0,0);
+  line(0, 0, 400, 400);
+
+  stroke(211,0,0);
+  strokeWeight(1);
+  fill(112, 210, 128)
+  triangle(100, 123, 10, 123, 50, 200)
+
+  stroke(10,230,239);
+  fill(19, 80, 160)
+  rect(102, 300, 28, 19)
+
+  stroke(102,129,0);
+  fill(100, 210, 28)
+  quad(281, 234, 29, 291, 54, 28, 188, 201)
+
+  stroke('red');
+  fill('yellow')
+  ellipse(500, 32, 400, 300)
+}
