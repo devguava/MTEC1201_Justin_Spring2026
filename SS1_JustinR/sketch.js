1726ced1b35af8377dc971f711ab942621d1fe08
devguava <demongamer2006.2@gmail.com>
2026-02-19 03:59:48 -0500

    Updated

--- SS1_JustinR/sketch.js
+++ SS1_JustinR/sketch.js
@@ -1,29 +1,99 @@
 /*
 Short Study by Justin Roldan
+Sketch Title: SPY!!
+With this sketch I've made. I want to explore interactive arts, like games or reactive art works.
 */
 function setup()
 {
-  createCanvas(400, 400);
-  background(255, 120, 50);
+  createCanvas(1280, 720);
+  background(140, 91, 106);
 
-  strokeWeight(8);
-  stroke(211,0,0);
-  line(0, 0, 400, 400);
+  //Red
+  fill(184,56,59);
+  beginShape();
 
-  stroke(211,0,0);
-  strokeWeight(1);
-  fill(112, 210, 128)
-  triangle(100, 123, 10, 123, 50, 200)
+  vertex(500, 720);
+  vertex(420, 640);
+  vertex(390, 530);
+  vertex(340, 420);
+  vertex(340, 300);
+  vertex(370, 280);
+  vertex(370, 240);
+  vertex(460, 100);
+  vertex(620, 0);
+  vertex(640, 0);
+  vertex(640, 720);
 
-  stroke(10,230,239);
-  fill(19, 80, 160)
-  rect(102, 300, 28, 19)
+  endShape(CLOSE);
 
-  stroke(102,129,0);
-  fill(100, 210, 28)
-  quad(281, 234, 29, 291, 54, 28, 188, 201)
+  //Blue
+  fill(88,133,162);
+  beginShape();
 
-  stroke('red');
-  fill('yellow')
-  ellipse(500, 32, 400, 300)
+  vertex(500 + 300, 720);
+  vertex(420 + 450, 640);
+  vertex(390 + 500, 530);
+  vertex(340 + 580, 420);
+  vertex(340 + 580, 300);
+  vertex(370 + 510, 280);
+  vertex(370 + 510, 240);
+  vertex(460 + 375, 100);
+  vertex(600 + 60, 0);
+  vertex(640, 0);
+  vertex(640, 720);
+
+  endShape(CLOSE);
+
+  //eyehole
+  fill(247, 171, 145);
+  beginShape();
+
+  vertex(500, 370);
+  vertex(390, 310);
+  vertex(420, 260);
+  vertex(820, 260);
+  vertex(860, 310);
+  vertex(750, 370);
+  vertex(670, 330);
+  vertex(610, 330);
+
+  endShape(CLOSE);
+
+  //mouthhole
+  fill(247, 171, 145);
+  beginShape();
+  
+  vertex(500, 520);
+  vertex(465, 560);
+  vertex(465, 600);
+  vertex(500, 640);
+  vertex(800, 640);
+  vertex(825, 600);
+  vertex(825, 560);
+  vertex(800, 520);
+  vertex(675, 560);
+  vertex(600, 560);
+
+  endShape(CLOSE);
+
+  //Red Logo
+  fill(184,56,59);
+  rect(150, 200, 175, 350);
+  strokeWeight(15)
+  stroke(0)
+  textSize(100)
+  text('R', 200, 300);
+  text('E', 200, 400);
+  text('D', 200, 500);
+
+  //Red Logo
+  fill(88,133,162);
+  strokeWeight(1)
+  rect(950, 200, 175, 350);
+  stroke(0)
+  strokeWeight(15)
+  textSize(100)
+  text('B', 1000, 300);
+  text('L', 1000, 400);
+  text('U', 1000, 500);
 }
