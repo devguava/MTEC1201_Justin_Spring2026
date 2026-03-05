703abe53be71379aceca6f1c25afce4215f204bb
devguava <demongamer2006.2@gmail.com>
2026-03-05 16:03:19 -0500

    added

---
+++ SS4_JustinR/sketch.js
@@ -0,0 +1,25 @@
+/*
+Name: Justin Roldan
+Title: Escape Shrek's Swamp
+Description: In this sketch, you have an objective. Escape from Shrek's swamp. It's a survival puzzle game where the swamp is laid out as a maze. 
+Control: W to move upward, S to move downward, A to move left, D to move down
+*/
+
+// let img;
+
+// function preload()
+// {
+//   image = loadImage('/assets/')
+// }
+
+function setup()
+{
+  createCanvas(1280, 720);
+  beginShape;
+  
+}
+
+function draw()
+{
+  background(220);
+}
