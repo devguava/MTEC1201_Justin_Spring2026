fbc624b718fc9c8bf325f81121279e34cc2e91d2
devguava <demongamer2006.2@gmail.com>
2026-03-12 04:02:15 -0400

    Updated Sketch

--- SS4_JustinR/sketch.js
+++ SS4_JustinR/sketch.js
@@ -5,11 +5,16 @@ Description: In this sketch, you have an objective. Escape from Shrek's swamp. I
 Control: W to move upward, S to move downward, A to move left, D to move down
 */
 
+/*
+Reference:
+"Move with Arrow Keys with p5.js"[https://youtu.be/SatRryVpAKE]
+*/
 // let img;
 let speedX = 5;
 let speedY = 5;
-let playerX = 500;
-let playerY = 500;
+let playerX = 28;
+let playerY = 635;
+let playerD = 50;
 // function preload()
 // {
 //   image = loadImage('/assets/')
@@ -24,25 +29,63 @@ function setup()
 function draw()
 {
   background(155, 169, 92);
-  beginShape();
-  vertex(10,10);
-  endShape(CLOSE);
   fill(192,75,101);
   ellipseMode(CENTER);
 
   //player
-  ellipse(playerX,playerY,50);
+  ellipse(playerX,playerY,playerD);
 
-  //player movement
-  if (keyIsPressed === true) {
-    if (keyCode === UP_ARROW) {
+  //player Movement
+  //Controller input gets confused when pressing the buttons simultaneously. Making the player continuously move the same direction that was made previously.
+  if (keyIsPressed) {
+    if (keyCode === UP_ARROW)
+    {
       playerY -= speedY;
-    } else if (keyCode === DOWN_ARROW) {
+    } else if (keyCode === DOWN_ARROW)
+    {
       playerY += speedY;
-    } else if (keyCode === LEFT_ARROW) {
+    }
+    if (keyCode === LEFT_ARROW)
+    {
       playerX -= speedX;
-    } else if (keyCode === RIGHT_ARROW) {
+    } else if (keyCode === RIGHT_ARROW)
+    {
       playerX += speedX;
     }
   }
+
+  //Level Layout
+  fill(45,54,24)
+  
+  beginShape();
+  vertex(1160,0);
+  vertex(0,0);
+  vertex(0,600);
+  vertex(50,600);
+  vertex(50,50);
+  vertex(1160,50);
+  endShape(CLOSE);
+
+  beginShape();
+  vertex(1230,0);
+  vertex(1280,0);
+  vertex(1280,720);
+  vertex(0,720);
+  vertex(0,670);
+  vertex(1230,670);
+  endShape(CLOSE);
+
+  beginShape();
+  vertex(170,125);
+  vertex(170,600);
+  vertex(120,600);
+  vertex(120,125);
+  endShape(CLOSE);
+
+  beginShape();
+  vertex(200,125);
+  vertex(200,600);
+  vertex(150,600);
+  vertex(150,125);
+  endShape(CLOSE);
 }
