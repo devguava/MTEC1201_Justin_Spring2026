5cfd99d19fff2077e77ae0bf7229c806ecb4699b
devguava <demongamer2006.2@gmail.com>
2026-03-26 16:21:59 -0400

    Updates Sketch

--- Midterm_JustinR/sketch.js
+++ Midterm_JustinR/sketch.js
@@ -1,7 +1,7 @@
 /*
 Midterm Project
 Sketch Title: Case Chase by Justin Roldan
-This project is gonna be a small interactive game where you have to avoid being grabbed for a certain amount of time.
+This project is gonna be a small interactive game where you have to avoid being caught for a certain amount of time. Use WASD to move the character.
 
 References:
 [Changing scenes P5 JS] https://www.youtube.com/watch?v=RlsRQS5qFSY (This demostarts how to add and change scene to scene)
@@ -14,14 +14,13 @@ let startButtonXsize = 400;
 let startButtonYsize = 100;
 let player;
 let chaser;
-let playerX = 120
-let playerY = 125
-let chaserX = 1115
-let chaserY = 125
+let playerX = 120;
+let playerY = 125;
+let chaserX = 1115;
+let chaserY = 125;
 //This varible is a map to our key presses. It is currently set as an empty Java object.
 let pressedKeys = {};
 let scene = 0;
-
 function setup(){
 	createCanvas(1280,720);
 	player = new Player(playerX,playerY);
@@ -29,13 +28,15 @@ function setup(){
 }
 
 function draw(){
-	print('"Mouse Axis" X:',mouseX,'Y:',mouseY);
+	//print('"Mouse Axis" X:',mouseX,'Y:',mouseY);
     if(scene === 0){
 	    titleScene();
     }else if(scene === 1){
         gameScene();
     }else if(scene === 2){
 		gameOver();
+	}else if(scene === 3){
+		gameWin();
 	}
 }
 
@@ -66,6 +67,7 @@ function mousePressed(){
 }
 
 function gameScene(){
+	print('Time:', millis()/1000)
 	scene = 1;
     background(203,153,96);
 	player.update();
@@ -75,7 +77,13 @@ function gameScene(){
 	chaserX = chaserX + ((playerX - chaserX) * 5);
     chaserY = chaserY + ((playerY - chaserY) * 5);
 	if(playerX > chaserX - (75/2) && playerX < chaserX + (75/2) && playerY > chaserY - (75/2) && playerY < chaserY + (75/2)){
+		print('contact')
 		scene = 2
+	}
+
+	if(millis()/1000 > 15){
+		print('Time Up')
+		scene = 3
 	}
 }
 
@@ -99,19 +107,19 @@ class Player{
 		let mvmt = createVector(0,0);
 		if (pressedKeys.w){
 			mvmt.y -= 1;
-			print('Up');
+			//print('Up');
 		}
 		if (pressedKeys.s){
 			mvmt.y += 1;
-			print('Down');
+			//print('Down');
 		}
 		if (pressedKeys.a){
 			mvmt.x -= 1;
-			print('Left');
+			//print('Left');
 		}
 		if (pressedKeys.d){
 			mvmt.x += 1;
-			print('Right');
+			//print('Right');
 		}
 		//this sets the movement's magnitude to be capped to it's speed. This helps avoid us going over our top speed when going diagonally.
 		mvmt.setMag(this.speed);
@@ -138,12 +146,19 @@ class Chaser{
 		this.y += mvmt.y;
 	}
 	draw(){
-		fill(255,0,0)
+		fill(255,0,0);
 	    circle(this.x,this.y,75);
 	}
 }
 
 function gameOver(){
-	scene = 2
-	background(0,0,0)
+	scene = 2;
+	background(0,0,0);
+	text("You've been caught!",width/2,height/2);
+}
+
+function gameWin(){
+	scene = 3;
+	background(0,255,0);
+	text('You Made It Out!',width/2,height/2);
 }
