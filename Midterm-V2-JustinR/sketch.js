0d4be92c0e32e4680c60f36f95d9822b416658eb
devguava <demongamer2006.2@gmail.com>
2026-04-16 05:58:37 -0400

    Updated sketch

--- Midterm-V2-JustinR/sketch.js
+++ Midterm-V2-JustinR/sketch.js
@@ -5,7 +5,9 @@ This project is gonna be a small interactive game where you have to avoid being 
 
 References:
 [Changing scenes P5 JS] https://www.youtube.com/watch?v=RlsRQS5qFSY (This demostarts how to add and change scene to scene)
+[16.1: let vs var - Topics of JavaScript/ES6] https://youtu.be/q8SHaDQdul0 (Explains the difference between varibles "var" and "let")
 [Beginner's Guide: Make things move with keyboard input in P5js] https://www.youtube.com/watch?v=MA_aFQV9vss (This demostrates how to properly control our character and using classes)
+[timer example by lillylin] https://editor.p5js.org/lillylin/sketches/2-YC_bac1 (Demostartes how to restart a timer between scenes)
 */
 
 let startButtonXposition = 1280/2;
@@ -20,61 +22,85 @@ let chaserX = 1115;
 let chaserY = 125;
 //This varible is a map to our key presses. It is currently set as an empty Java object.
 let pressedKeys = {};
-let scene = 0;
+var scene = 0;
+let startTime = 0;
+let setTime;
+let countDownTimer = 10
 
 function setup(){
+	frameRate(60)
 	createCanvas(1280,720);
 	player = new Player(playerX,playerY);
 	chaser = new Chaser(chaserX,chaserY);
 }
 
 function draw(){
+	print('scene: ', scene);
+	print('Time:', millis()/1000);
 	//print('"Mouse Axis" X:',mouseX,'Y:',mouseY);
-    if(scene === 0){
-	    titleScene();
-    }else if(scene === 1){
+    if(scene == 0){
+		titleScene();
+    }else if(scene == 1){
         gameScene();
-    }else if(scene === 2){
+    }else if(scene == 2){
 		gameOver();
-	}else if(scene === 3){
+	}else if(scene == 3){
 		gameWin();
 	}
 }
 
+function startMenu(){
+	scene = 0
+}
 function titleScene(){
-	// scene = 0
+	setTime = 0
+	countDownTimer = setTime
     background(107,166,249);
     rectMode(CENTER);
+	fill(255,255,255);
     rect(startButtonXposition,startButtonYposition,startButtonXsize,startButtonYsize);
     textAlign(CENTER);
     textSize(100);
+	fill(0,0,0);
     text('CASE CHASE',width/2,200);
     textSize(75);
     text('START',width/2,385);
 	if(mouseX > 440 && mouseX < 840 && mouseY > 310 && mouseY < 410){
         //print('in');
-	    rect(startButtonXposition,startButtonYposition,startButtonXsize + 25,startButtonYsize + 25)
+		fill(255,255,255);
+	    rect(startButtonXposition,startButtonYposition,startButtonXsize + 25,startButtonYsize + 25);
+		fill(0,0,0);
 	    textSize(100);
         text('START',width/2,395);
 	}else{
 	    //print('out');
 	}
+
+    if (millis()/1000-startTime >= setTime){
+		startTime = millis()/1000;
+	}
+	
+	if(millis()/1000<setTime){ //first scene
+	   }else if(millis()/1000>= setTime && millis()/1000<= setTime + (setTime * 2)){//second scene
+    }
+
 }
 
 function mousePressed(){
 	if(scene == 0 && mouseX > 440 && mouseX < 840 && mouseY > 310 && mouseY < 410){
-		print('Game Start');
-		gameScene();
+		startGame();
 	}
-	// if(scene == 3 && mouseX > 440 && mouseX < 840 && mouseY > 310 && mouseY < 410){
-	// 	print('Main Menu');
-	// 	titleScene();
-	// }
+	if(scene == 3 && mouseX > 440 && mouseX < 840 && mouseY > 310 && mouseY < 410){
+		startMenu();
+	}
 }
 
+function startGame(){
+	scene = 1
+}
 function gameScene(){
-	print('Time:', millis()/1000)
-	scene = 1;
+	setTime = 10
+	countDownTimer = setTime
     background(203,153,96);
 	player.update();
 	player.draw();
@@ -82,15 +108,24 @@ function gameScene(){
 	chaser.draw();
 	chaserX = chaserX + ((playerX - chaserX) * 5);
     chaserY = chaserY + ((playerY - chaserY) * 5);
-	if(playerX > chaserX - (75/2) && playerX < chaserX + (75/2) && playerY > chaserY - (75/2) && playerY < chaserY + (75/2)){
-		print('contact')
-		scene = 2
+	fill(0)
+	textSize(50)
+	text("Time: " + int(countDownTimer), 100, 50)
+	
+	if (countDownTimer >= startTime){
+		countDownTimer -= 0.02
 	}
 
-	if(millis()/1000 > 15){
-		print('Time Up')
-		scene = 3
+    if (millis()/1000-startTime >= setTime){
+		print('Time Up');
+		startTime = millis()/1000;
+		scene = 3;
+		countDownTimer = setTime
 	}
+	
+	if(millis()/1000<setTime){
+	   }else if(millis()/1000>= setTime && millis()/1000<= setTime + (setTime * 2)){
+    }	
 }
 
 function keyPressed() {
@@ -157,14 +192,29 @@ class Chaser{
 	}
 }
 
-function gameOver(){
+function startOver(){
 	scene = 2;
+}
+
+function gameOver(){
+	setTime = 0
 	background(0,0,0);
 	text("You've been caught!",width/2,height/2);
+	if (millis()/1000-startTime >= setTime){
+		startTime = millis()/1000;
+	}
+	
+	if(millis()/1000<setTime){ //first scene
+	   }else if(millis()/1000>= setTime && millis()/1000<= setTime + (setTime * 2)){//second scene
+    }
+}
+
+function startWin(){
+	scene = 3;
 }
 
 function gameWin(){
-	scene = 3;
+	setTime = 0
 	background(0,255,0);
     rectMode(CENTER);
 	fill(255,255,255)
@@ -182,7 +232,16 @@ function gameWin(){
 		fill(0,0,0)
 	    textSize(75);
 		text('Main Menu',width/2,385);
+
 	}else{
 	    //print('out');
 	}
+
+	if (millis()/1000-startTime >= setTime){
+		startTime = millis()/1000;
+	}
+	
+	if(millis()/1000<setTime){ //first scene
+	   }else if(millis()/1000>= setTime && millis()/1000<= setTime + (setTime * 2)){//second scene
+    }
 }
