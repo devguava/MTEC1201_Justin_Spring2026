72c6efa2ab6d55660c7fe541906ad3b1bfe9fcb9
devguava <demongamer2006.2@gmail.com>
2026-04-21 02:01:11 -0400

    Updated sketch

--- Short Studies/SS5-JustinR/sketch.js
+++ Short Studies/SS5-JustinR/sketch.js
@@ -1,7 +1,39 @@
+/*
+Title: 
+Author: Justin Roldan
+reference: 
+Ball move down for loopby cs4all [https://editor.p5js.org/cs4all/sketches/Hyv07I6nZ] (An example of moving ellipses using for loops)
+for()loop Rectangles by annA [https://editor.p5js.org/annA/sketches/7JUx_st6p] (An example in creating a for loop for rectangles)
+*/
+
+let x;
+let y;
+let speed;
+let rectSize;
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(800, 800);
 }
 
 function draw() {
-  background(220);
+  x = 400;
+  y = 400;
+  speed = 10;
+  rectSize = 100
+
+  background(255);
+  stroke(5);
+
+  for(let x = 0; x < width; x += rectSize) {
+    for(let y = 0; y < height; y += 40) {
+    fill(200);
+    rect(x, y, rectSize, 40);
+    }
+  }
+
+  for (let xMover = 0; xMover <= width; xMover += 10){
+      fill(255, 191, 0)
+      ellipse(x + xMover,y,20)
+      y += speed;
+      x += speed;
+  }
 }
