0a68b9db8d6b20c352b83d134d0f03cd00aab62b
devguava <demongamer2006.2@gmail.com>
2026-05-21 14:16:59 -0400

    updated

--- Short Studies/Final-JustinR/sketch.js
+++ Short Studies/Final-JustinR/sketch.js
@@ -2,7 +2,7 @@
 Title: Space Hunt
 By: Justin Roldan
 Input: Click the left mouse button on the red circle(UFOs) to capture them.
-Goal: Capture all five UFOs
+Goal: Capture all five UFOs before it goes off screen from above.
 */
 
 let startButtonXposition = 1280/2;
@@ -15,7 +15,7 @@ let target2;
 let target3;
 let target4;
 let button;
-let scene = 0;
+let scene = 4;
 let captured = 0;
 let requiredCapture;
 let gameMusic;
@@ -24,6 +24,8 @@ let victoryMusic;
 let mainMenuMusic;
 let victoryhasPlayed = false;
 let gameOverhasPlayed = false;
+let shootSound;
+let destroySound;
 // let timer;
 // let interval;
 // let startTime;
@@ -31,7 +33,9 @@ let gameOverhasPlayed = false;
 
 
 function preload(){
-	soundFormats('mp3')
+	soundFormats('mp3', 'wav')
+	shootSound = loadSound('audio/sfx/Shoot.wav');
+    destroySound = loadSound('audio/sfx/Destroy.wav');
 	gameMusic = loadSound('audio/music/Background Music.mp3');
     gameOverMusic = loadSound('audio/music/Game Over.mp3');
     victoryMusic = loadSound('audio/music/Victory!.mp3');
@@ -39,6 +43,8 @@ function preload(){
 }
 
 function setup(){
+	shootSound.setVolume(0.5)
+	destroySound.setVolume(0.75)
 	gameMusic.setVolume(0.5);
 	gameOverMusic.setVolume(0.5);
 	victoryMusic.setVolume(0.5);
@@ -54,7 +60,7 @@ function setup(){
 function draw(){
 	//print('scene: ', scene);
 	//print('Global Time:', int(millis()/1000);
-	//print('"Mouse Axis" X:',mouseX,'Y:',mouseY);
+	print('"Mouse Axis" X:',mouseX,'Y:',mouseY);
 	if(scene == 0){
 		titleScene();
 	}else if(scene == 1){
@@ -63,6 +69,8 @@ function draw(){
 		gameOver();
 	}else if(scene == 3){
 		niceWork();
+	}else if(scene == 4){
+		information();
 	}
 }
 
@@ -87,6 +95,11 @@ function titleScene(){
 	text('Space Hunt',width/2,200);
 	textSize(75);
 	text('START',width/2,385);
+	textSize(65)
+	fill(255,255,255);
+	rect(1280/2,(720/2) + 165,400,100);
+	fill(0,0,0);
+	text('Information',width/2,550);
 	if(mouseX > 440 && mouseX < 840 && mouseY > 310 && mouseY < 410){
 		//print('in');
 		fill(255,255,255);
@@ -94,6 +107,16 @@ function titleScene(){
 		fill(0,0,0);
 		textSize(100);
 		text('START',width/2,395);
+	}else{
+		//print('out');
+	}
+	if(mouseX > 440 && mouseX < 840 && mouseY > 475 && mouseY < 575){
+		//print('in');
+		fill(255,255,255);
+		rect(1280/2,(720/2) + 165,400 + 75,100 + 50);
+		fill(0,0,0);
+		textSize(90);
+		text('Information',width/2,555)
 	}else{
 		//print('out');
 	}
@@ -105,6 +128,7 @@ function mousePressed(){
 	}
 
 	if(scene == 1){
+		shootSound.play()
 		target0.shot();
 		target1.shot();
 		target2.shot();
@@ -116,6 +140,10 @@ function mousePressed(){
 	if(scene == 2 && mouseX > 440 && mouseX < 840 && mouseY > 310 && mouseY < 410){
 		victoryMusic.stop()
 		startMenu();
+	}
+
+	if(scene == 0 && mouseX > 440 && mouseX < 840 && mouseY > 475 && mouseY < 575){
+		info();
 	}
 
 	if(scene == 3 && mouseX > 440 && mouseX < 840 && mouseY > 310 && mouseY < 410){
@@ -150,6 +178,15 @@ function gameScene(){
 	target4.draw();
 	fill(161,161,161);
 	ellipse(width/2, 720, 1290, 200);
+	fill(100,100,100);
+	ellipse(width/2, 0, 1290, 50);
+	fill(0,255,0,100);
+	beginShape();
+	vertex((1280/2 - 250), 0);
+	vertex(0, 720);
+	vertex(1280, 720);
+	vertex((1280/2 + 250), 0);
+	endShape(CLOSE);
 	
 
 	if(captured == requiredCapture){
@@ -194,6 +231,7 @@ class Targets{
 			this.ySpeed *= 0
 			this.colorG = 255
 			this.colorR = 0
+			destroySound.play()
 			captured += 1
 			print("Got 'em");
 			
@@ -268,4 +306,19 @@ function niceWork(){
 	}else{
 		//print('out');
 	}
+}
+
+function info(){
+	scene = 4;
+}
+
+function information(){
+	rectMode(CENTER);
+	background(0,0,40);
+	fill(0,255,255,105);
+	square(320,400,600)
+	fill(255,255,255);
+	textAlign(LEFT)
+	textSize(30)
+	text("Story:\n\nHello, Soldier.\n Your mission today. Take out those UFOs that have been ease dropping on our research facility. Show them what we do to those who get nosy.\n\nInstructions:\nLeft click on the targets using your mouse to capture those UFOs. Don't let them fly back to the ship, that's their exit.", 325, 400, 550, 550)
 }
